5080ee36379c292af0382b7264da5fd2d2a80b50
Abigail-Ashley-Cayco <123426509+Abigail-Ashley-Cayco@users.noreply.github.com>
2023-04-20 07:46:40 +0800

    Add files via upload

---
+++ hahaha.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+def toggle_color():
+    if label["bg"] == "blue":
+        label.config(bg="yellow")
+    else:
+        label.config(bg="blue")
+
+root = tk.Tk()
+root.title("Main Window")
+label = tk.Label(root, text="Color",fg='red', bg="blue")
+label.pack(pady=50)
+
+button = tk.Button(root, text="Click to change the color of the button", command=toggle_color)
+button.pack(pady=10)
+
+root.mainloop()
