d8f430eb1322d7f2f945f0d61216bc773d0e5099
DESPI-Cj <123426631+DESPI-Cj@users.noreply.github.com>
2023-05-02 09:38:28 +0800

    Update Laboratory No.6.py

--- Laboratory No.6.py
+++ Laboratory No.6.py
@@ -10,11 +10,13 @@ class Table:
         self.num_rows = 4
     def create_table(self):
         #Create a^1, a^2, a^3
-        for i in range(1,self.num_rows_a + 1):
-            label_text = "a^" + str(i) + "   "
-            label = Label(text = label_text)
-            label.grid(row = 0, column= i)
-        #Create table for a^1
+        label1 = Label(text = "a")
+        label1.grid(row = 0, column= 1)
+        label2 = Label(text= "a^2")
+        label2.grid(row=0, column=2)
+        label3 = Label(text="a^3")
+        label3.grid(row=0, column=3)
+        #Create table for a
         for i in range(1,self.num_rows + 1):
             label_text1 = str(i)
             label1 = Label(text = label_text1)
@@ -29,6 +31,7 @@ class Table:
             label_text1 = int(i) * int(i) * int(i)
             label1 = Label(text=str(label_text1))
             label1.grid(row=i + 1, column=3)
+
 
 mywin = Table(window)
 mywin.create_table()
